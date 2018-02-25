16a2f0b34fe8e3493523761fa54957b49087bee4
emmann1 <emanuel.mares@gmail.com>
2018-02-25 03:56:51 +0200

    aded target blank to links

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var transcripts, schedule;
 function linkify(text) {
     var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
     return text.replace(urlRegex, function(url) {
-        return '<a href="' + url + '">' + url + '</a>';
+        return '<a href="' + url + ' " target="_blank">' + url + '</a>';
     });
 };
 function display(amaSession, questionId){
